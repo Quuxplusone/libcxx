a927e47a3ec341d8e1e465ffa0ae04690687990f
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2018-04-24 21:34:16 -0700

    Add the aliases `std::pmr::vector` and so on.
    This required some refactoring to avoid making <vector> depend on <mutex>.

---
+++ test/std/utilities/utility/mem.res/mem.res.aliases/header_list_synop2.pass.cpp
@@ -0,0 +1,28 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// REQUIRES: c++17
+
+// <list>
+
+// namespace std::pmr {
+//
+// typedef ... list
+//
+// } // namespace std::pmr
+
+#include <list>
+
+int main()
+{
+    {
+        // Check that std::pmr::list is usable without <memory_resource>.
+        std::pmr::list<int> l;
+    }
+}
