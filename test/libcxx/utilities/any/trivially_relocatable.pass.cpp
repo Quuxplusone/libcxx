51b5706fa13579467cba57ab26f86bcb18c89137
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2018-07-12 12:15:58 -0700

    Low-hanging [[trivially_relocatable]]: shared_ptr, weak_ptr, any.

---
+++ test/libcxx/utilities/any/trivially_relocatable.pass.cpp
@@ -0,0 +1,27 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// <any>
+//
+// class any
+
+#include <any>
+
+#if __has_extension(is_trivially_relocatable)
+
+class _LIBCPP_TRIVIALLY_RELOCATABLE Reloc { ~Reloc(); };
+
+static_assert(std::is_trivially_relocatable<int>::value, "");
+static_assert(std::is_trivially_relocatable<Reloc>::value, "");
+
+static_assert(std::is_trivially_relocatable< std::any >::value, "");
+
+#endif
+
+int main() {}
