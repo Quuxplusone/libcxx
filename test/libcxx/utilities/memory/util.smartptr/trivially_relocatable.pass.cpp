407daac007468460d269afcdf07bb88f9e2048cd
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2018-07-12 12:15:58 -0700

    Low-hanging [[trivially_relocatable]]: shared_ptr, weak_ptr, any.

---
+++ test/libcxx/utilities/memory/util.smartptr/trivially_relocatable.pass.cpp
@@ -0,0 +1,39 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// UNSUPPORTED: c++03
+//
+// <memory>
+//
+// class shared_ptr
+// class weak_ptr
+
+#include <memory>
+
+class Incomplete;
+class NonTriv { ~NonTriv(); };
+class _LIBCPP_TRIVIALLY_RELOCATABLE Reloc { ~Reloc(); };
+
+static_assert(std::is_trivially_relocatable<int>::value, "");
+static_assert(!std::is_trivially_relocatable<NonTriv>::value, "");
+#if __has_extension(is_trivially_relocatable)
+static_assert(std::is_trivially_relocatable<Reloc>::value, "");
+#endif
+
+#if __has_extension(is_trivially_relocatable)
+static_assert(std::is_trivially_relocatable< std::shared_ptr<int> >::value, "");
+static_assert(std::is_trivially_relocatable< std::shared_ptr<Incomplete> >::value, "");
+static_assert(std::is_trivially_relocatable< std::shared_ptr<NonTriv> >::value, "");
+
+static_assert(std::is_trivially_relocatable< std::weak_ptr<int> >::value, "");
+static_assert(std::is_trivially_relocatable< std::weak_ptr<Incomplete> >::value, "");
+static_assert(std::is_trivially_relocatable< std::weak_ptr<NonTriv> >::value, "");
+#endif
+
+int main() {}
