f8dacbcdac15a20855b3509608dffebba6fedffc
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2019-02-04 22:38:13 -0500

    Add a fast `replace_top` method to `std::priority_queue`.
    Also `std::poke_heap(first, last)`, which assumes that the given range
    is in max-heap order *except* for the first element, which needs to be
    put into its proper place. This is just like `push_heap`, except that
    `push_heap` assumes the *last* element is out of order, whereas
    `poke_heap` assumes the *first* element is out of order.
    
    The changes in `<queue>` could use `_VSTD::poke_heap` instead of
    `__sift_down`, but they do not, in order to keep the `<queue>` changes
    usable as a single-file patch (in case someone wants to take the
    `<queue>` changes without the `<algorithm>` changes).
    
    The `poke_heap` approach would replace the last line of each
    new method with this line instead:
    
        _VSTD::poke_heap(c.begin(), c.end(), comp);
    
    https://reviews.llvm.org/D57734

---
+++ test/libcxx/containers/container.adaptors/priority.queue/priqueue.members/replace_top.pass.cpp
@@ -0,0 +1,31 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <queue>
+
+// priority_queue();
+
+// void replace_top(const value_type& v);
+
+#include <queue>
+#include <cassert>
+
+int main(int, char**)
+{
+    std::priority_queue<int> q;
+    q.push(1);
+    assert(q.top() == 1);
+    q.push(3);
+    assert(q.top() == 3);
+    q.replace_top(2);
+    assert(q.top() == 2);
+    q.replace_top(0);
+    assert(q.top() == 1);
+
+  return 0;
+}
